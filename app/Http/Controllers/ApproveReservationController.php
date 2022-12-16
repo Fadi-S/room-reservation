425f8fbb22d80e16ef9795f7bbade065db069324
Fadi Sarwat <fadisarwat@yahoo.com>
2022-12-16 13:30:30 +0200

    Add protection from duplicate events

--- app/Http/Controllers/ApproveReservationController.php
+++ app/Http/Controllers/ApproveReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ReservationResource;
 use App\Models\Reservation;
+use App\Rules\RoomAvailableRule;
 use Illuminate\Http\Request;
 
 class ApproveReservationController extends Controller
@@ -26,6 +27,22 @@ class ApproveReservationController extends Controller
     public function approve(Reservation $reservation)
     {
         $this->authorize("admin");
+
+        $available = new RoomAvailableRule(
+            $reservation->room_id,
+            $reservation->date,
+            $reservation->day_of_week,
+            $reservation->start,
+            $reservation->end,
+            $reservation->id,
+        );
+
+        if (!$available->passes(null, null)) {
+            session()->flash("message", $available->message());
+            session()->flash("type", "error");
+
+            return back();
+        }
 
         if ($reservation->approve()) {
             session()->flash("message", "تم الموافقة علي الميعاد");
