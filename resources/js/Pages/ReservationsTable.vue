7ab9dc03238e3abbdf5693c4f4437988c578bcae
Fadi Sarwat <fadisarwat@yahoo.com>
2022-12-16 18:30:45 +0200

    WIP

--- resources/js/Pages/ReservationsTable.vue
+++ resources/js/Pages/ReservationsTable.vue
@@ -3,7 +3,7 @@
 
     <Tabs v-model="day" :tabs="nextWeekDays" />
 
-    <div class="overflow-x-auto touch-auto bg-white">
+    <div class="overflow-x-auto touch-pinch-zoom bg-white">
         <table class="mt-3">
             <colgroup span="2"></colgroup>
             <colgroup span="2"></colgroup>
